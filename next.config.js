dfbc115507eac2ff836eb3ef23003e0e26205bd5
zuhdy-ev <ardhiekov@gmail.com>
2026-06-28 02:57:07 +0700

    fix image loader path

--- next.config.js
+++ next.config.js
@@ -1,19 +1,42 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: '/2024/gelex',
+
+//   output: 'export',
+//   trailingSlash: true,
+
+//   images: {
+//     loader: 'custom',
+//     path: 'http://localhost:3005/2024/gelex',
+//     loaderFile: './src/components/loader.js',
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost:3005',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'drive.google.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'firebasestorage.googleapis.com',
+//       },
+//     ],
+//   },
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/2024/gelex',
-
   output: 'export',
   trailingSlash: true,
 
   images: {
     loader: 'custom',
-    path: 'http://localhost:3005/2024/gelex',
     loaderFile: './src/components/loader.js',
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost:3005',
-      },
       {
         protocol: 'https',
         hostname: 'drive.google.com',
--- src/components/loader.js
+++ src/components/loader.js
@@ -1,12 +1,25 @@
+// import PropTypes from 'prop-types'
+
+// export function akamaiLoader({ src, width }) {
+//   return `${process.env.NEXT_PUBLIC_BASE_URL}${src}?imwidth=${width}`
+// }
+
+// akamaiLoader.PropTypes = {
+//   src: PropTypes.string.isRequired,
+//   width: PropTypes.string.isRequired,
+// }
+
+// export default akamaiLoader
+
 import PropTypes from 'prop-types'
 
 export function akamaiLoader({ src, width }) {
-  return `${process.env.NEXT_PUBLIC_BASE_URL}${src}?imwidth=${width}`
+  return `${src}?imwidth=${width}`
 }
 
-akamaiLoader.PropTypes = {
+akamaiLoader.propTypes = {
   src: PropTypes.string.isRequired,
-  width: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
 }
 
 export default akamaiLoader

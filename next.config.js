b75bfcd5540130961342a4ce273c1ff6fbb18e77
zuhdy-ev <ardhiekov@gmail.com>
2026-06-28 02:14:59 +0700

    change next config basepath

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/2024/gelex',
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/2024/gelex',
-        permanent: true,
-        basePath: false, // Prevents Next.js from prefixing the source with the basePath
-      },
-    ]
-  },
+
   output: 'export',
   trailingSlash: true,
+
   images: {
     loader: 'custom',
     path: 'http://localhost:3005/2024/gelex',
@@ -21,12 +13,10 @@ const nextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost:3005',
-        port: '',
       },
       {
         protocol: 'https',
         hostname: 'drive.google.com',
-        port: '',
       },
       {
         protocol: 'https',
